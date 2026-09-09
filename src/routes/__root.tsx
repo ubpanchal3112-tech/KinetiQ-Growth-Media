6f3b1516d02f0eb85e46e34dc4c299b35b779004
kostechsoft-eng <kostechsoft@gmail.com>
2026-09-09 21:38:28 +0530

    fil chnaged

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -103,7 +103,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/logo.jpeg", type: "image/x-icon" },
     ],
   }),
   shellComponent: RootShell,
